f44ab0e888768bfb702ff06726a177e6faf729e7
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-16 08:25:39 +0530

    create signin, signup, showtodo and some other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
-export default function App() {
-  return <div className="text-center">Hello World</div>;
-}
+// src/App.tsx
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import DarkModeToggle from "./components/DarkModeToggle";
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
+      <BrowserRouter>
+        <header className="p-4">
+          <DarkModeToggle />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
